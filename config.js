5fc3c7b55710bdff5f8d44f9c17f47f97fdbdfa4
Cami688 <camila.calderon.o@icloud.com>
2025-02-12 20:03:35 +1000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dx2349pv6/raw/upload/v1739353711/watch_scczjo", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dx2349pv6/raw/upload/v1739354492/j5lqk3d1dlajoyilkbsq", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
